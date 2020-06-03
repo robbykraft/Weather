06fe1be13b4c7b2194ee446471f2932da9b65223
Robby Kraft <robbykraft@gmail.com>
2020-06-03 08:12:56 -0400

    incorporate rain effect. and temperature brightness if > 10 degree difference

--- src/main.ino
+++ src/main.ino
@@ -15,7 +15,6 @@
 #include "LedControl.h"
 
 // this needs to match the query string from the proxy server
-// todo: fill in the password. removed for security
 #define PROXY_QUERY_PASSWORD "/?password="
 #define PIXEL_PIN D0
 #define PIXEL_COUNT 24
@@ -32,9 +31,12 @@ LedControl sevenSegment = LedControl(SPI_DATA, SPI_CLOCK, SPI_CHIP_SELECT, 1);
 // data from the weather API. initialize to zero
 double relativeTemp[24];
 double precip[24];
+double splash[24];
 unsigned long lastAPICall = 0;
 bool switchHigh = false;
-unsigned long intervalAPI = 1000 * 60 * 60 * 1; // 1 hour
+// unsigned long intervalAPI = 1000 * 60 * 60 * 1; // 1 hour
+unsigned long intervalAPI = 1000 * 60 * 30; // 30 min
+// unsigned long intervalAPI = 1000 * 60 * 1; // 1 min
 unsigned long halfInterval = intervalAPI / 2.0;
 
 // never compare against millis(). only modulus of millis()
@@ -70,9 +72,10 @@ void callAPI () {
   request.hostname = "weather-clock-proxy.herokuapp.com";
   request.port = 80;
   request.path = PROXY_QUERY_PASSWORD;
+  //request.body = "{\"key\":\"value\"}";
 
   http.get(request, response, headers);
-  Particle.publish("API response", String(response.status));
+  // Particle.publish("API response", String(response.status));
 
   delay(200);
   StaticJsonDocument<4800> doc;
@@ -82,38 +85,51 @@ void callAPI () {
     return;
   }
   double temp0 = doc[0]["temperature"];
-  
   // update seven segment
   int tens = temp0 / 10.0;
   int ones = ((int)temp0) % 10;
   if (temp0 > 100) { tens = 0; ones = 0; }
   sevenSegment.setDigit(0,1,tens,false);
   sevenSegment.setDigit(0,0,ones,false);
-  
   // update data for the neopixel ring
   for (int i = 0; i < 24; i++) {
     double thisTemp = doc[i]["temperature"]; // .as<float>();
+    double thisPrecip = doc[i]["precipProbability"];
     // const char* summary = doc[i]["summary"];
     relativeTemp[i] = thisTemp - temp0;
-    precip[i] = doc[i]["precipProbability"];
+    if      (thisPrecip > 0.35) { precip[i] = 50; }
+    else if (thisPrecip > 0.2)  { precip[i] = 10; }
+    else                        { precip[i] = 0; }
   }
   String temp_list = "";
   for (int i = 0; i < 24; i++) {
     temp_list += String(relativeTemp[i]) + ", ";
   }
-  Particle.publish("temperature list", temp_list);
+  // Particle.publish("temperature list", temp_list);
 }
 
 void lightRing () {
   float seconds = millis() / 1000.0;
-  double wave = sin(seconds / 2.0) * 15.0;
+  double hueWave = sin(seconds * 0.1) * 15.0;
+  double panelBright = 0.5 + sin(seconds * 0.1 / 3.0) * 0.2;
+  // update rain splash effect
   for (int i = 0; i < strip.numPixels(); i++) {
-    byte hue = (relativeTemp[i] > 0) ? hue = 80 + wave : hue = 175 + wave;
-    float brightness = fabs(relativeTemp[i]);
-    if (brightness > 1.0) { brightness = 1.0; }
+    if (rand() % 1000 < precip[i]) { splash[i] = 0;  }
+    if (splash[i] < 1.0) { splash[i] += 0.025; }
+    else { splash[i] = 1.0; }
+  }
+  // set pixel colors
+  for (int i = 0; i < strip.numPixels(); i++) {
+    byte hue = (relativeTemp[i] > 0) ? hue = 80 + hueWave : hue = 175 + hueWave;
+    // float temperatureBright = fabs(relativeTemp[i]);
+    // if (temperatureBright > 1.0) { temperatureBright = 1.0; }
+    float temperatureBright = fabs(relativeTemp[i]) < 10 ? 0.2 : 1.0;
+    float brightness = temperatureBright * splash[i] * panelBright;
     strip.setPixelColor(i, colorHue(hue % 255, brightness));
   }
-  strip.setPixelColor(0, strip.Color(255, 255, 255));
+  // set top pixel
+  byte whiteBright = 255 * splash[0] * panelBright * 0.5;
+  strip.setPixelColor(0, strip.Color(whiteBright, whiteBright, whiteBright));
   strip.show();
 }
 
@@ -122,6 +138,7 @@ void setup () {
   for (int i = 0; i < 24; i++) {
     relativeTemp[i] = 1;
     precip[i] = 0;
+    splash[i] = 1;
   }
   // reset seven segment
   sevenSegment.shutdown(0,false);
@@ -138,10 +155,10 @@ void loop () {
   lightRing();
   delay(50);
   if (checkTimer()) {
-    for (int i = 0; i < 24; i++) {
-      strip.setPixelColor(i, strip.Color(255, 255, 255));
-    }
-    strip.show();
+    // for (int i = 0; i < 24; i++) {
+    //   strip.setPixelColor(i, strip.Color(255, 255, 255));
+    // }
+    // strip.show();
     callAPI();
   }
 }
